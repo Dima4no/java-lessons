60bf24e1a037e7881a439e0a79fb899bccaeeb5c
Dima <nds081989@hmail.com>
2024-11-23 18:48:03 +0800

    The homework is completed!

---
+++ src/homework/inheritance/Archer.java
@@ -0,0 +1,30 @@
+package homework.inheritance;
+
+public class Archer extends Character {
+
+    private int arrows;
+
+    public Archer(String name, int health, int arrows) {
+        super(name, health);
+        this.arrows = arrows;
+    }
+
+    public int getArrows() {return arrows;}
+    public void setArrows(int arrows) {this.arrows = arrows;}
+
+    public void attack(Character target) {
+        if (arrows > 0) {
+            System.out.println("Archer " + getName() + " shoots an arrow at " + target.getName() +
+                    ", dealing 15 damage! " + (arrows--) + " arrows left.");
+            target.setHealth(target.getHealth() - 15);
+            arrows--;
+        } else {
+            System.out.println("Archer " + getName() + " has no arrows left!");
+        }
+    }
+
+    public void reload(int amount) {
+        arrows += amount;
+        System.out.println("Archer " + getName() + " reloads " + amount + " arrows." + "Total arrows: " + arrows);
+    }
+}
---
+++ src/homework/inheritance/Character.java
@@ -0,0 +1,38 @@
+package homework.inheritance;
+
+public class Character {
+
+    private String name;
+    private int health;
+
+    public Character(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public String getName() {return name;}
+    public int getHealth() {return health;}
+
+    public void setName(String name) {this.name = name;}
+    public void setHealth(int health) {this.health = health;}
+
+    public void attack(Character target) {
+        System.out.println("Character " + name + " attacks " + target.name  + "!");
+        target.health -= 10;
+    }
+
+    public boolean isAlive() {
+        if (health > 0) {return true;} else {return false;}
+    }
+
+    public void heal(int amount) {
+        health += amount;
+        System.out.println("Character: " + name + " heals for " + amount + " points!");
+    }
+
+    @Override
+    public String toString() {
+        return "Character: " + name + "\n" +
+                "Health: " + health;
+    }
+}
