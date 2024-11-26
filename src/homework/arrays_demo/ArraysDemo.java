39e3da3ea815692aa0702a14f49f82670791fb3b
mariaprang <mprangishvili@newwork.com>
2024-11-26 17:50:07 +0300

    added some draft for array topic

---
+++ src/homework/arrays_demo/ArraysDemo.java
@@ -0,0 +1,28 @@
+package homework.arrays_demo;
+
+public class ArraysDemo {
+
+    // CREATE, ASSIGN, READ,
+    public static void main(String[] args) {
+        // 1. CREATE
+        // - Создать массив -
+        // Если мы не знаем заранее значений, кот. будут нах-ся в array -
+        // data type [ ] name = new data type [size];
+        int [] numbers = new int[3]; // [0][0][0]
+        // Если не знаем заранее значений, кот. будут нах-ся в array -
+        // data type [ ] name = {value1, value2, value3};
+        String [] names = {"Tom", "Alice", "Sam"}; // ["Tom"]["Alice"]["Sam"]
+        // 2. ASSIGN -
+        // - Присвоить значения - name[index] = value;
+        numbers[1] = 20;
+        // [0][20][0]
+        //  0   1  2
+        numbers[2] = 30; // [0][20][30]
+        // WARNING: ---> numbers[3] = 40; // ArrayIndexOutOfBoundsException
+        // 3. READ -
+        // - Прочитать значения - name[index];
+        System.out.println(numbers[1]); // 20
+        System.out.println(names[2]); // Sam
+    }
+
+}
